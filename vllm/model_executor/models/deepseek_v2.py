bc6dd3dcb9553c45a003755157dd86c3b0aa228f
Woosuk Kwon <woosuk@inferact.ai>
2026-03-30 05:28:13 +0000

    all reduce + residual + rms norm
    Signed-off-by: Woosuk Kwon <woosuk@inferact.ai>

--- vllm/model_executor/models/deepseek_v2.py
+++ vllm/model_executor/models/deepseek_v2.py
@@ -385,11 +385,12 @@ class DeepseekV2MoE(nn.Module):
                 hidden_states=hidden_states, router_logits=router_logits
             )
 
-        if self.is_sequence_parallel:
-            final_hidden_states = tensor_model_parallel_all_gather(
-                final_hidden_states, 0
-            )
-            final_hidden_states = final_hidden_states[:num_tokens]
+        if not getattr(self, "skip_final_allreduce", False):
+            if self.is_sequence_parallel:
+                final_hidden_states = tensor_model_parallel_all_gather(
+                    final_hidden_states, 0
+                )
+                final_hidden_states = final_hidden_states[:num_tokens]
 
         return final_hidden_states.view(num_tokens, hidden_dim)
 

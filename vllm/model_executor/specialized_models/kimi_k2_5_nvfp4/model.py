0c37897a0a2510907c0f012050b9e90659bfe34c
robin <robin>
2026-04-12 10:27:35 +0000

    cutedsl rope

--- vllm/model_executor/specialized_models/kimi_k2_5_nvfp4/model.py
+++ vllm/model_executor/specialized_models/kimi_k2_5_nvfp4/model.py
@@ -2,8 +2,6 @@
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
 """Text-only specialized Kimi-K2.5 NVFP4 implementation."""
 
-from __future__ import annotations
-
 from collections.abc import Iterable
 
 import torch
@@ -504,48 +502,57 @@ class KimiK25Nvfp4MLAAttention(nn.Module):
 
 import cutlass
 import cutlass.cute as cute
+from cutlass.cute.runtime import from_dlpack, make_ptr
 
 # requires:
 #  - not neox style
 #  - rotary dim = head size
 @cute.kernel
 def kimik25_rope_kernel(
-    positions: cute.Tensor, # (Sp, R)
-    query: cute.Tensor, # (2, Sp, N_local, R // 2)
-    key: cute.Tensor, # (2, Sp, 1, R // 2)
-    cos_sin_cache: cute.Tensor, # (R // 2, max_position_embeddings, 2)
+    positions: cute.Tensor, # (Sp,)
+    query: cute.Tensor, # (Sp, N_local, (2, R // 2))
+    key: cute.Tensor, # (Sp, 1, (2, R // 2))
+    cos_sin_cache: cute.Tensor, # (max_position_embeddings, (R // 2, 2))
 ):
     tid, _, _ = cute.arch.thread_idx()
     bid, _, _ = cute.arch.block_idx()
 
-    cache_row = cos_sin_cache[tid, positions[bid]]
-    cos = cache_row[0]
-    sin = cache_row[1]
+    cache_row = cos_sin_cache[positions[bid], (tid, None)]
+    cos, sin = cache_row[0], cache_row[1]
     
-    for head in range(query.shape[2]):
-        query_slice = query[None, bid, head, tid]
-        a, b = query_slice.load()
-        a = a * cos - b * sin
-        b = a * sin + b * cos
-        query_slice.store([a, b])
-    key_slice = key[None, bid, 0, tid]
-    a, b = key_slice.load()
-    a = a * cos - b * sin
-    b = a * sin + b * cos
-    key_slice.store([a, b])
+    for head in range(query.shape[1]):
+        query_slice = query[bid, head, (None, tid)]
+        v = query_slice.load()
+        a, b = v[0], v[1]
+        a_new = a * cos - b * sin
+        b_new = a * sin + b * cos
+        query_slice[0] = a_new
+        query_slice[1] = b_new
+    key_slice = key[bid, 0, (None, tid)]
+    v = key_slice.load()
+    a, b = v[0], v[1]
+    a_new = a * cos - b * sin
+    b_new = a * sin + b * cos
+    key_slice[0] = a_new
+    key_slice[1] = b_new
 
 @cute.jit
 def kimik25_rope(
-    positions: cute.Tensor, # (Sp, R)
-    query: cute.Tensor, # (Sp, N_local, R)
+    positions: cute.Tensor, # (Sp,)
+    query: cute.Tensor, # (Sp, N_local, R):(?, ?, 1)
     key: cute.Tensor, # (Sp, 1, R)
     cos_sin_cache: cute.Tensor, # (max_position_embeddings, R)
+    N_local: cutlass.Constexpr,
+    half_rope_dim: cutlass.Constexpr
 ):
-    query = cute.logical_divide(query, cute.Shape((1, 1, 2)))
-    key = cute.logical_divide(key, cute.Shape((1, 1, 2)))
-    sp, _, R = query.shape
-    cos_sin_cache = cute.logical_divide(cos_sin_cache, cute.Shape(R // 2))
-    kimik25_rope_kernel(positions, query, key, cos_sin_cache).launch(grid=(sp, 1, 1), block=(R // 2, 1, 1))
+    sp = positions.shape[0]
+    query = cute.make_tensor(
+        query.iterator,
+        cute.make_layout((sp, N_local, (2, half_rope_dim)), stride=((query.stride[0], query.stride[1], (1, 2))))
+    )
+    key = cute.logical_divide(key, (1, 1, 2))[(0, None), (0, None), None]
+    cos_sin_cache = cute.logical_divide(cos_sin_cache, (1, half_rope_dim))[(0, None), None]
+    kimik25_rope_kernel(positions, query, key, cos_sin_cache).launch(grid=(sp, 1, 1), block=(half_rope_dim, 1, 1))
 
 class ForkedKimiK25Nvfp4MLAAttention(KimiK25Nvfp4MLAAttention):
     """Forked MLA path for kernel benchmarking experiments."""
@@ -571,7 +578,13 @@ class ForkedKimiK25Nvfp4MLAAttention(KimiK25Nvfp4MLAAttention):
         q_pe = q[..., self.qk_nope_head_dim :]
         k_pe = k_pe.unsqueeze(1)
         # q_pe, k_pe = self.rotary_emb(positions, q_pe, k_pe)
-        kimik25_rope(positions, q_pe, k_pe, self.rotary_emb.cos_sin_cache)
+
+        assert(q_pe.dtype == self.rotary_emb.cos_sin_cache.dtype)
+        positions_cute = from_dlpack(positions)
+        q_pe_cute = from_dlpack(q_pe).mark_layout_dynamic()
+        k_pe_cute = from_dlpack(k_pe)
+        cos_sin_cache_cute = from_dlpack(self.rotary_emb.cos_sin_cache)
+        kimik25_rope(positions_cute, q_pe_cute, k_pe_cute, cos_sin_cache_cute, self.num_local_heads, self.qk_rope_head_dim // 2)
         q[..., self.qk_nope_head_dim :] = q_pe
 
         mla = self.mla_attn
